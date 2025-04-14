bdcabf1b6acc29fd51aca5a82cb52d0b684e4f95
Michael <minh.pham@wordbee.com>
2025-04-14 14:32:28 +0700

    Improve1

--- Plugins/WordBeeEditor/Source/WordBeeEditor/Utils/Constants.cpp
+++
@@ -1,6 +0,0 @@
-#include "Constants.h"
-#include "Misc/Paths.h"
-
-const FString Constants::UserDataPath = FPaths::ProjectSavedDir() + "/Wordbee/UserData";
-const FString Constants::DocumentDataPath = TEXT("/Game/Plugins/YourPlugin/DocumentDataAsset");
-
